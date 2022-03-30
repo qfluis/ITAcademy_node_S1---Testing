32085a623aef76fbf674f05d92806c9f4f06b217
Luis Quevedo <qfluis@gmail.com>
2022-03-30 12:12:32 +0200

    Entrega 1.6 a revisión

--- app/nivel2_Animales.js
+++ app/nivel2_Animales.js
@@ -1,20 +1,3 @@
-// Nivel 2 Ejercicio 2
-/* Crea una classe Persona que rebi un paràmetre 'nom' al ser instanciada. 
-La classe inclourà un mètode dirNom que imprimeixi per consola el paràmetre 'nom'. 
-Invoca el mètode dirNom des de fora de la classe. */
-
-class Persona {
-    constructor(nom){
-        this.nom = nom;
-    }
-
-    dirNom(){
-        const saludo = `El meu nom es: ${this.nom}`;
-        console.log(saludo);
-        return saludo;    
-    }
-}
-
 // Nivel 3 Ejercicio 1
 /* Escriu una function creadora d'objectes que faci instàncies d'una classe abstracta. 
 Invoca-la amb diferents definicions. */
@@ -36,5 +19,5 @@ const crearAnimal = (tipo, nombre) => {
     return new Animal(tipo, nombre);
 }
 
-module.exports.Persona = Persona;
+module.exports.Animal = Animal;
 module.exports.crearAnimal = crearAnimal;
